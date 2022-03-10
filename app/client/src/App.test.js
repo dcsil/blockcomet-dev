284dbf7210400e4a54b4a7dc63988fbc9b6b9856
ShahmirU <shahmirusman1999@gmail.com>
2022-03-10 00:37:33 -0500

    Update jest workflow

--- app/client/src/App.test.js
+++ app/client/src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+function helloWorld(){
+  return "hello world"
+}
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test hello world', () => {
+  expect(helloWorld()).toEqual("hello world");
 });
