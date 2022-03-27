cf7ad6834bcee7467ccfb9781d4aec49b19518c8
Shahmir Usman <shahmirusman1999@gmail.com>
2022-03-27 00:28:02 -0400

    Finished main page

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.png';
+import logo from './blockcomet_logo_no_name.png';
+import { Form, Container, Row, Col, Button } from 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import {useState} from 'react' 
 function App() {
@@ -7,22 +10,24 @@ function App() {
   const [productDetails, setProductDetails] = useState('')
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-input">
-            <div>
-            Product Name
-          </div>
-          <input value={productName} onInput={e => {setProductName(e.target.value)}}>
-          </input>
-          
-          <button type="button" class="example-btn">
-            Create Digital Asset 
-          </button>
-          </div>
-        </header>
-      </div>
+        <div className="App">        
+          <Container>
+            <Row className="align-items-center">
+              <Col className="align-items-right">
+                <img src={logo} className="logo-img" alt="logo" />
+              </Col>
+              <Col>
+                {/* <input type="text" className="searchTerm" placeholder="Enter Product ID"/> */}
+                <Form>
+                <Form.Control className="search-bar" size="lg" type="text" placeholder="Enter Product ID"/>
+                </Form>
+              </Col>
+              <Col> 
+                <Button className="search-btn" variant="primary" size="lg"> Validate </Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
     )
 }
 
