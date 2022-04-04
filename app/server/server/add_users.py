82fc6d69ae5fb6d1c8b530373139d89c7ae605f8
Chinmaya Nathany <chinmaya.nathany@gmail.com>
2022-04-03 22:26:27 -0400

    Add a test user

--- app/server/server/add_users.py
+++ app/server/server/add_users.py
@@ -3,8 +3,7 @@ from passlib.hash import bcrypt
 
 #Connecting to our mongoDB instance in cloud
 cluster = MongoClient("mongodb+srv://admin:admin@users.d0ihk.mongodb.net/blockcomet?retryWrites=true&w=majority")
-# db = cluster['blockcomet']
 db = cluster['blockcomet']
 table = db['users']
-post = {"username": "rohit", "password": bcrypt.hash('passwordroh')}
+post = {"username": "test", "password": bcrypt.hash('test123')}
 table.insert_one(post)
