09f45e7dcc4af33c9eeb6e218c754bacef8670ec
Rohit Bansal <rohitbansal0602@yahoo.com>
2022-04-04 13:27:23 -0400

    Create Page works, added field functionality, hooked up to API

--- app/client/src/AdminNavigationbar.js
+++ app/client/src/AdminNavigationbar.js
@@ -2,11 +2,24 @@ import { Navbar, Button, Container } from 'react-bootstrap';
 import './css/Navigationbar.css';
 import logo from './assets/blockcomet_logo_no_name.png';
 import { Link } from "react-router-dom";
+import axios from 'axios'
+import { serverUrl } from './config'
+import useToken from './useToken'
 
 function Navigationbar(props) {
-
+    const { token } = useToken()
     const onLogout = () => {
         localStorage.removeItem('token')
+        axios({
+            method: 'get',
+            url: `${serverUrl}/logout`,
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+            .then((response) => {
+                console.log("logout response", response)
+            }).catch((err) => {
+                console.log("logout error", err)
+            })
     }
 
     return (
