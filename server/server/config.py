04e2e016d0936f61db4b88c069e84e299a6507fb
Chinmaya Nathany <chinmaya.nathany@gmail.com>
2022-04-05 15:41:06 -0400

    Update frontend deployment url

--- server/server/config.py
+++ server/server/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     DB_COL_NAME: str = 'blockcomet'
     DB_URI: str = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_NAME}.d0ihk.mongodb.net/{DB_COL_NAME}?retryWrites=true&w=majority"
     SIGNATURE: str = 'blockcomet_mvp'
-    FRONTEND_URL: str = 'https://blockcometwebsite-6dkam7pfeq-ue.a.run.app'
+    FRONTEND_URL: str = 'https://blockcomet-frontend-6dkam7pfeq-uc.a.run.app'
     BDB_URL = 'https://test.ipdb.io'
 
     # 60 minutes * 24 hours * 8 days = 8 days
