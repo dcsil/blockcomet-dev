f6f832b6789d350783bceeb5e22969366d2556f4
Shahmir Usman <shahmirusman1999@gmail.com>
2022-04-05 15:54:28 -0400

    Hooked up API to validation pages

--- app/client/src/Validate.js
+++ app/client/src/Validate.js
@@ -1,27 +1,23 @@
-import { Form, Container, Row, Col, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './css/Validate.css';
-import { useState } from 'react'
-import { useParams } from "react-router-dom"
-import Navigationbar from './Navigationbar'
+import React from "react";
+import axios from 'axios';
+import { serverUrl } from './config';
+import Navigationbar from './Navigationbar';
 import success from './assets/success.png';
 import failure from './assets/failure.png';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 function Success(props) {
-    const products = [props.productDetails.data]
-    const columns = [
-        { text: 'UID', dataField: 'UID' },
-        { text: 'Company', dataField: 'company' },
-        { text: 'Manufacturer', dataField: 'manufacturer' },
-        { text: 'Model', dataField: 'model' }
-      ];
-
+    const columns = [{'text': 'hi', 'dataField': 'hi'},
+                    {'text': 'yo', 'dataField': 'yo'}]
+    const productDetails = [{'hi': 1, 'yo': 2}]
     return ( 
         <div className="Success">
             <Container className="align-items-center">  
                     <img src={success} className="success-img" alt="success" data-testid="success" />
-                    <i><h4>Verified by <b>{props.productDetails.data.manufacturer}</b></h4></i>
-                    <BootstrapTable columns={columns} data={products} keyField='uid' />
+                    <i><h4>Verified by <b>{props.productDetails[0].Brand}</b></h4></i>
+                    <BootstrapTable columns={props.columns} data={props.productDetails} keyField='Product ID' />
             </Container>
         </div>   
     )
@@ -38,39 +34,64 @@ function Failure(props) {
     )
 }
 
-function Validate() {
-    const [productName, setProductName] = useState('')
-    const [manufacturerName, setManufacturerName] = useState('')
-    const [productDetails, setProductDetails] = useState('')
-    let params = useParams()
-    const productID = params.id;
-    // Make API Call with productID
-    // axios.get(`https://blockcometapi-6dkam7pfeq-uc.a.run.app/`)
-    // .then(res => {
-    // })
-    const res = {'data':
-        {'company': 'blockcomet',
-        'manufacturer': 'Rolex SA',
-        'model': 'Submariner',
-        'UID': '123'
-        },
-        'statusCode': 200
+class Validate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            productID: window.location.pathname.substring(10),
+            status: null,
+            productDetails: null,
+            columns: null,
+        };
+    }
+    componentDidMount() {
+        this.loadData();
     }
 
-    if (res.statusCode == 200) {
-        return (
-            <div>
-            <Navigationbar backPage="/"/>
-            <Success productDetails={res}/>
-            </div>
-        )
+    loadData() {
+        axios.get(`${serverUrl}/get_product/${this.state.productID}`)
+        .then(res => {
+            if (res.status == 200) {
+                const productInfo = {};
+                const columns = [];
+                for (let i = 0; i < res.data.data.product_data.length; i++) {
+                    productInfo[res.data.data.product_data[i].key] = res.data.data.product_data[i].value;
+                    columns.push({'text': res.data.data.product_data[i].key, 'dataField': res.data.data.product_data[i].key});
+                }
+                this.setState({
+                    status: 'Success',
+                    productDetails: [productInfo],
+                    columns: columns
+                });
+            }
+        }).catch((error) => {
+            this.setState({
+                status: 'Failure'
+            });
+        })
     }
-    return (
-        <div>
-        <Navigationbar backPage="/"/>
-        <Failure productID={productID}/>
-        </div>
-    )
+
+    render() {
+        if (!this.state.status) {
+            return <div />
+        }
+        else if (this.state.status === 'Success') {
+            return (
+                <div>
+                <Navigationbar backPage="/"/>
+                <Success productDetails={this.state.productDetails} columns={this.state.columns}/>
+                </div>
+            )
+        }
+        else if (this.state.status === 'Failure') {
+            return (
+                <div>
+                <Navigationbar backPage="/"/>
+                <Failure productID={this.state.productID}/>
+                </div>
+            )
+        }
+    }
 }
 
 export default Validate;
